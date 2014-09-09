18e3f3691f971328eb3f4dc5bd931b103662dfdb
oliver short <45074@longroad.ac.uk>
2014-09-09 15:12:30 +0100

    variables

---
+++ exercise 1.2  - variables.py
@@ -0,0 +1,11 @@
+#oliver short
+#09/09/14
+#exercise 1.2 variables
+
+print("hello world")
+#the '=' sign is reffered to 'assinged to' rather than 'equals'
+first_name = input("oliver: ")
+#the first name is a variable 
+print(first_name)
+print("hi {0} !".format(first_name) )
+
