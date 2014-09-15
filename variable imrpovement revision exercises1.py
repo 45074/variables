4e718819f6580675212056c15611884c68355384
oliver short <45074@longroad.ac.uk>
2014-09-15 12:07:49 +0100

    variable improvement revision exercise 1

---
+++ variable imrpovement revision exercises1.py
@@ -0,0 +1,14 @@
+#oliver short
+#15/09/14
+#exercise: variable improvement revision exercises
+
+
+
+number1 = int(input("what is your first number"))
+number2 = int(input("what is your second number"))
+number3 = int(input("what is your third number"))
+number4 = int(input("whatis your forth number"))
+
+answer = number1 + number2 + number3 + number4
+
+print (answer)
